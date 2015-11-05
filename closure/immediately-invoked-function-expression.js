9c2ca361615e360cd70b72d20124aa995d5c1435
theseafoxx <tkhrabsky@gmail.com>
2015-11-04 21:15:05 -0500

    Update immediately-invoked-function-expression.js

--- closure/immediately-invoked-function-expression.js
+++ closure/immediately-invoked-function-expression.js
@@ -1,6 +1,13 @@
 /* Fun with functions
  * Immediately-invoked function expression (IIFE) 
- * A self-invoked anonymous function executed in the context of the window
+ * A self-invoked anonymous function executed in the context of the window.
+ * This expression is most useful when attempting to preserve the global 
+ * namespace as any variables declared within the function body will be local 
+ * to the closure but will still live throughout runtime. This is a popular 
+ * means of encapsulating source code for applications and frameworks, typically 
+ * exposing a single global interface in which to interact with.
+ *
+ * http://ryanmorr.com/understanding-scope-and-context-in-javascript/
  */
 
 // Module is added to the window object.
