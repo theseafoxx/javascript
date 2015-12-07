53cde6bbd268b31cc412ffb3bf53adbb3b3054c2
Tanya <tkhrabsky@gmail.com>
2015-12-07 17:25:13 -0500

    Update getParentObject.js

--- toolbox/getParentObject.js
+++ toolbox/getParentObject.js
@@ -1,6 +1,6 @@
 /*
- * Return the parent object based on the parent tag that is being looked for
- * if the parent had a class name or id then you probably would not use this search
+ * Return the parent tag object based on the parent tag that is being looked for.
+ * If the parent had a class name or id then you probably would not use this search.
  * Condition: I found the item(s) and now I need a specific parent node to affect a change to.
  * Example: There is an <ol> and some <li> have an icon, I grab all the icon objects then need 
  * to get at each parent <li>.
